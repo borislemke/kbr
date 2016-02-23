848fc7446c47cc2e9a7a8f2838137a9d7b2afb6f
gusman widodo <gusmanwidodo@gmail.com>
2016-02-23 10:55:40 +0800

    fix admin navbar

--- resources/views/admin/fragments/navbar.blade.php
+++ resources/views/admin/fragments/navbar.blade.php
@@ -35,21 +35,39 @@ function childIsActive($children, $link) {
     }
 }
 
+$nav_category = (isset($_GET['category'])) ? $_GET['category'] : '' ;
+
+switch ($nav_category) {
+    case 'villa':
+        $nav_category = 'villas';
+        break;
+    case 'land':
+        $nav_category = 'lands';
+        break;
+    case 'villa-rental':
+        $nav_category = 'villa rental';
+        break;
+    
+    default:
+        $nav_category = '';
+        break;
+}
+
 ?>
 
 <ul id="navbar-main" class="flexbox">
 
     <?php foreach($navigation as $nav => $navlinks): ?>
-    <li class="navbar-main-item"><a href class="navbar-main-link<?php childIsActive($navlinks, $link) ?>"><?= str_replace('_', ' ', $nav) ?></a></li>
+    <li class="navbar-main-item"><a href class="navbar-main-link<?php childIsActive($navlinks, $link) ?> <?=($link == 'properties' && $nav == $nav_category) ? 'active' : ''; ?>"><?= str_replace('_', ' ', $nav) ?></a></li>
     <?php $first = FALSE; endforeach ?>
 </ul>
 
 <div id="navbar-wrapper">
 
     <?php foreach($navigation as $nav => $navlinks): ?>
-    <ul class="navbar-ul flexbox <?php childIsActive($navlinks, $link) ?>">
+    <ul class="navbar-ul flexbox <?php childIsActive($navlinks, $link) ?> <?=($link == 'properties' && $nav == $nav_category) ? 'active' : ''; ?>">
         <?php foreach($navlinks as $linkUrl => $linkName): ?>
-        <li class="navbar-item"><a class="<?= $linkUrl == $link ? 'active' :''; ?>" href="{{ URL::to('/') }}/admin/<?= $linkUrl ?>"><?= $linkName ?></a></li>
+        <li class="navbar-item"><a class="<?= $linkUrl == $link ? 'active' :''; ?><?=($link == 'properties' && (isset($_GET['status']) ? $_GET['status'] : '') == $linkName) ? 'active' : ''; ?>" href="{{ URL::to('/') }}/admin/<?= $linkUrl ?>"><?= $linkName ?></a></li>
         <?php endforeach ?>
     </ul>
     <?php endforeach ?>
